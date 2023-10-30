64bb847817cfc9005d1a192ecc22279588fb014a
waldensanchez <walden.sanch@gmail.com>
2023-10-29 21:23:02 -0600

    Ahorcado.py finished

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,58 +1,80 @@
-import random
+WORD = 'WORDS'
+LIVES = 5
 
 class AhorcadoGame:
-    def __init__(self, word, lives=5):
+    def __init__(self, word, lives):
         self.word = word.upper()
         self.lives = lives
         self.guesses = []
         self.current_word = ['_'] * len(word)
 
     def display_state(self):
-        print("AHORCADO:")
+        print("\nAHORCADO:")
         print(f"WORD: {' '.join(self.current_word)}")
         print(f"LIVES: {'X ' * self.lives}")
         print(f"LETTERS USED: {' '.join(self.guesses)}")
 
-    def check_win(self):
+    def is_in_word(self, guess):
+        return guess in self.word
+
+    def remove_lives(self):
+        self.lives -= 1
+
+    def did_i_win(self):
         return ''.join(self.current_word) == self.word
 
-    def play(self):
-        while self.lives > 0 and not self.check_win():
-            self.display_state()
-            guess = input("NEXT GUESS? ").strip().upper()
+    def already_used(self, guess):
+        return guess in self.guesses
 
-            if len(guess) != 1 or not 'A' <= guess <= 'Z' or guess == 'Ñ':
-                print("INVALID GUESS")
+    def valid_guess(self, guess):
+        if len(guess) != 1 or not guess.isalpha() or guess == 'Ñ':
+            print("INVALID GUESS")
+            return False
+        return True
+
+    def get_guess(self):
+        while True:
+            guess = input("NEXT GUESS?").strip().upper()
+            if not self.valid_guess(guess):
                 continue
-
-            if guess in self.guesses:
-                self.lives -= 1
+            if self.already_used(guess):
                 print("YOU DUMB!")
-            elif guess in self.word:
-                for i in range(len(self.word)):
-                    if self.word[i] == guess:
-                        self.current_word[i] = guess
-                self.guesses.append(guess)
+                self.remove_lives()
+                self.display_state()
+                if self.lives == 0:
+                    return None
             else:
-                self.lives -= 1
-                self.guesses.append(guess)
-                print("NOT IN WORD!")
+                return guess
 
-        self.display_state()
-        if self.check_win():
+    def update_current_word(self, guess):
+        for i in range(len(self.word)):
+            if self.word[i] == guess:
+                self.current_word[i] = guess
+
+    def game_over_message(self):
+        if self.did_i_win():
             print("YOU WIN!")
         else:
             print("YOU LOST!")
 
-def assign_word():
-    word = input("Enter a word to guess: ").strip().upper()
-    return word
+    def play(self):
+        while self.lives > 0 and not self.did_i_win():
+            self.display_state()
+            guess = self.get_guess()
+            if guess is None: 
+                break
+            self.guesses.append(guess)
+
+            if self.is_in_word(guess):
+                self.update_current_word(guess)
+            else:
+                self.remove_lives()
+                print("NOT IN WORD!")
+
+        self.game_over_message()
 
 def main():
-    # Configuration part
-    WORD = assign_word()  # Assign a word at the beginning
-    LIVES = 5  # Change the number of lives if needed
-
+    global WORD, LIVES
     game = AhorcadoGame(WORD, LIVES)
     game.play()
 
--- Ahorcado_Game.py
+++ Ahorcado_Game.py
@@ -1,7 +1,9 @@
+import requests
+import re
 import random
 
 class AhorcadoGame:
-    def __init__(self, word, lives=5):
+    def __init__(self, word, lives):
         self.word = word.upper()
         self.lives = lives
         self.guesses = []
@@ -16,70 +18,117 @@ class AhorcadoGame:
             "  ____\n |    |\n |    O\n |   /|\n |\n |\n=========",
             "  ____\n |    |\n |    O\n |   /|\\\n |\n |\n=========",
             "  ____\n |    |\n |    O\n |   /|\\\n |   / \n |\n=========",
-            "  ____\n |    |\n |    O\n |   /|\\\n |   / \\ \n |\n========="  # Extended for longer words
+            "  ____\n |    |\n |    O\n |   /|\\\n |   / \\ \n |\n========="
         ]
-
-        print("AHORCADO:")
+        print("\nAHORCADO:")
         print(f"WORD: {' '.join(self.current_word)}")
         print(f"LIVES: {'X ' * self.lives}")
         print(f"LETTERS USED: {' '.join(self.guesses)}")
-
-        if self.hangman_stage >= 0 and self.hangman_stage < len(hangman_images):
-            print(hangman_images[self.hangman_stage])
-        else:
-            print("Hangman stage not available")
+        print(hangman_images[self.hangman_stage])
 
     def check_win(self):
         return ''.join(self.current_word) == self.word
 
+    def get_guess(self):
+        while True:
+            guess = input("NEXT GUESS? ").strip().upper()
+            if len(guess) != 1 or not guess.isalpha() or guess == 'Ñ':
+                print("INVALID GUESS")
+            elif guess in self.guesses:
+                print("YOU DUMB!")
+                self.lives -= 1
+                self.hangman_stage -= 1  # Update hangman image on repeat
+                self.display_state()
+                if self.lives == 0:
+                    return None
+            else:
+                return guess
+
+    def update_guesses_and_lives(self, guess):
+        if guess is None:
+            return
+        if guess in self.word:
+            for i in range(len(self.word)):
+                if self.word[i] == guess:
+                    self.current_word[i] = guess
+        else:
+            self.lives -= 1
+            print("NOT IN WORD!")
+            self.hangman_stage -= 1  # Update hangman image on wrong guess
+        self.guesses.append(guess)
+
+    def game_over_message(self):
+        if self.check_win():
+            print("YOU WIN!")
+            freed_hangman = (
+                "  ____\n |    |\n |   \\O/\n |    |\n |   / \\\n |\n========="
+            )
+            print(freed_hangman)
+        else:
+            print("YOU LOST!")
+
     def play(self):
         while self.lives > 0 and not self.check_win():
             self.display_state()
-            guess = input("NEXT GUESS? ").strip().upper()
-
-            if len(guess) != 1 or not 'A' <= guess <= 'Z' or guess == 'Ñ':
-                print("INVALID GUESS")
-                continue
-
-            if guess in self.guesses:
-                print("YOU DUMB! You already guessed this letter.")
-                continue
-
-            if guess in self.word:
-                for i in range(len(self.word)):
-                    if self.word[i] == guess:
-                        self.current_word[i] = guess
-                self.guesses.append(guess)
-            else:
-                self.lives -= 1
-                self.guesses.append(guess)
-                print("NOT IN WORD!")
-                self.hangman_stage -= 1  # Remove a limb
-
+            guess = self.get_guess()
+            if guess is None:
+                break
+            self.update_guesses_and_lives(guess)
         self.display_state()
-        if self.check_win():
-            self.display_win()
+        self.game_over_message()
+
+def fetch_wikipedia_article(topic):
+    topic = topic.replace(' ', '_')
+    url = f"https://en.wikipedia.org/w/api.php?action=query&titles={topic}&prop=extracts&format=json&explaintext"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        page_id = next(iter(data['query']['pages']))
+        extract = data['query']['pages'][page_id].get('extract', '')
+        return extract
+    else:
+        print("Failed to retrieve Wikipedia content")
+        return None
+
+def extract_words_from_text(text):
+    words = re.findall(r'\b[A-Za-z]{5,}\b', text)
+    return words
+
+def get_random_word(words):
+    long_words = [word for word in words if len(word) >= 5]
+    return random.choice(long_words).upper() if long_words else None
+
+def assign_word_from_wikipedia():
+    topic = input("Enter a topic for the hangman game: ")
+    article_content = fetch_wikipedia_article(topic)
+    if article_content:
+        words = extract_words_from_text(article_content)
+        word = get_random_word(words)
+        if word:
+            return word
         else:
-            print("YOU LOST! The word was:", self.word)
+            print("No suitable words were found in the article.")
+            return None
+    else:
+        print(f"Could not fetch a Wikipedia article on {topic}.")
+        return None
 
-    def display_win(self):
-        print("YOU WIN!")
-        freed_hangman = (
-            "  ____\n |    |\n |   \\O/\n |    |\n |   / \\\n |\n========="
-        )
-        print(freed_hangman)
-
-def assign_word():
-    word = input("Enter a word to guess: ").strip().upper()
-    return word
+def assign_lives():
+    lives = input("Enter the number of lives: ").strip()
+    if lives.isdigit():
+        return int(lives)
+    else:
+        print("Invalid input for lives. Using default of 5.")
+        return 5
 
 def main():
-    # Configuration part
-    WORD = assign_word()  # Assign a word at the beginning
-    LIVES = 6  # Change the number of lives if needed
-
-    game = AhorcadoGame(WORD, LIVES)
-    game.play()
+    WORD = assign_word_from_wikipedia()
+    if WORD:
+        LIVES = assign_lives()
+        game = AhorcadoGame(WORD, LIVES)
+        game.play()
+    else:
+        print("Game could not start without a word.")
 
 if __name__ == "__main__":
     main()
